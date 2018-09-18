a1a268803bcc34d774b571ecfdc1ad40fa151a29
Brian Vaughn <bvaughn@fb.com>
2018-09-18 09:00:44 -0700

    Fixed Webpack config

--- website/config/webpack.config.prod.js
+++ website/config/webpack.config.prod.js
@@ -154,6 +154,7 @@ module.exports = {
 
       // Load profiling-capable builds:
       'react-dom': 'react-dom/cjs/react-dom.profiling.min.js',
+      'schedule/tracking': 'schedule/cjs/schedule-tracking.profiling.min.js',
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
