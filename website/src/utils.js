2216e55f2e75e35a34c7b589358512b145245df0
Vamshi Bachaneboina <vamshi.bachaneboina@192.168.1.5>
2023-06-06 00:17:06 -0700

    remove type check for js and refactor code

--- website/src/utils.js
+++ website/src/utils.js
@@ -1,7 +1,4 @@
-export function fillArray(length, fillFunction): Array {
-  const array = new Array(length);
-  for (let i = 0; i < length; i++) {
-    array[i] = fillFunction(i);
-  }
-  return array;
-}
+// Create a new array with the length and fill it with the fillFunction
+export const fillArray = (length, fillFunction) => {
+  return Array.from({ length }, (_, i) => fillFunction(i));
+};
