4f903cc09494f30ee19f09a2c5ee15c8a7125113
Brian Vaughn <brian.david.vaughn@gmail.com>
2025-08-10 10:44:19 -0400

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
+import { defineConfig, type UserConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// https://vite.dev/config/
-export default defineConfig({
+const libraryConfig: UserConfig = {
   build: {
     lib: {
       entry: resolve(__dirname, "lib/index.ts"),
@@ -19,10 +18,37 @@ export default defineConfig({
       external: ["react", "react-dom"],
     },
   },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "react-window": resolve(__dirname, "lib"),
+    },
+  },
+};
+
+const websiteConfig: UserConfig = {
+  // build: {
+  //   outDir: "dist/docs",
+  // },
   plugins: [react(), svgr(), tailwindcss()],
   resolve: {
     alias: {
       "react-window": resolve(__dirname, "lib"),
     },
   },
-});
+};
+
+let config: UserConfig = {};
+switch (process.env.TARGET) {
+  case "lib": {
+    config = libraryConfig;
+    break;
+  }
+  default: {
+    config = websiteConfig;
+    break;
+  }
+}
+
+// https://vite.dev/config/
+export default defineConfig(config);
