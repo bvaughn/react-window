c98a3c5ef34a2fb0373354ae2126dee529abbb38
Brian Vaughn <brian.david.vaughn@gmail.com>
2025-09-08 04:46:59 -0400

    Add sponsor link to home page

--- src/routes/GettingStartedRoute.tsx
+++ src/routes/GettingStartedRoute.tsx
@@ -22,7 +22,7 @@ export default function GettingStartedRoute() {
         </ExternalLink>
         .
       </div>
-      <div className="text-xl">Installation</div>
+      <div className="text-xl mt-4">Installation</div>
       <div>Begin by installing the library from NPM:</div>
       <code className="grow text-xs md:text-sm block text-left whitespace-pre-wrap rounded-md p-3 bg-black text-white!">
         npm install <span className="tok-keyword">react-window</span>
@@ -32,6 +32,7 @@ export default function GettingStartedRoute() {
         <code>dist</code> folder and documentation is included within the{" "}
         <code>docs</code> folder.
       </Callout>
+      <div className="text-xl mt-4">Learn more</div>
       <div>
         This library provides two basic types of components; choose one below to
         learn more:
@@ -44,9 +45,26 @@ export default function GettingStartedRoute() {
           <Link to="/grid/grid">Grids</Link> (horizontal and vertical scrolling)
         </li>
       </ul>
-      <div>
-        Check out the <Link to="/support">support page</Link> if you need help.
-      </div>
+      <div className="text-xl mt-4">Support</div>
+      If you like this project there are several ways to support it:
+      <ul className="pl-8">
+        <li className="list-disc">
+          <ExternalLink href="https://github.com/sponsors/bvaughn/">
+            Become a GitHub sponsor
+          </ExternalLink>
+        </li>
+        <li className="list-disc">
+          <ExternalLink href="https://opencollective.com/react-window#sponsor">
+            Become an Open Collective sponsor
+          </ExternalLink>
+        </li>
+        <li className="list-disc">
+          or{" "}
+          <ExternalLink href="http://givebrian.coffee/">
+            buy me a coffee
+          </ExternalLink>
+        </li>
+      </ul>
     </Box>
   );
 }
