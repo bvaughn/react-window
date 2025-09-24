f0a6f7ac10abe28f8a3e260a6cc8272a8ee79e27
Brian Vaughn <brian.david.vaughn@gmail.com>
2025-09-24 19:14:53 -0400

    Example of variable height images

--- src/routes/grid/HorizontalListsRoute.tsx
+++ src/routes/grid/HorizontalListsRoute.tsx
@@ -3,6 +3,7 @@ import HorizontalListCellRendererMarkdown from "../../../public/generated/code-s
 import { Block } from "../../components/Block";
 import { Box } from "../../components/Box";
 import { Code } from "../../components/code/Code";
+import { Header } from "../../components/Header";
 import { LoadingSpinner } from "../../components/LoadingSpinner";
 import { HorizontalList } from "./examples/HorizontalList.example";
 import { useEmails } from "./hooks/useEmails";
@@ -12,6 +13,7 @@ export default function HorizontalListsRoute() {
 
   return (
     <Box direction="column" gap={4}>
+      <Header section="Other" title="Horizontal lists" />
       <div>A horizontal list is just a grid with only one row.</div>
       <div>Here's an example horizontal list (grid) of emails:</div>
       <Block className="h-20" data-focus-within="bold">
---
+++ src/routes/list/examples/Images.example.tsx
@@ -0,0 +1,73 @@
+import { RowComponent, type RowProps } from "./ImageRow.example";
+import type { Size } from "./useImageSizeCache.example";
+import { useImageSizeCache } from "./useImageSizeCache.example";
+
+// <begin>
+
+import { useCallback, useState } from "react";
+import { List } from "react-window";
+
+function Example({ images }: { images: string[] }) {
+  const [listSize, setListSize] = useState<Size>({ height: 0, width: 0 });
+
+  // Rows will lazily register image sizes in this cache
+  const cache = useImageSizeCache();
+
+  const rowHeight = useCallback(
+    (index: number) => {
+      const size = cache.getImageSize(index) ?? cache.getAverageSize();
+
+      // Scale the image to fit within the list
+      return listSize.width * (size.height / size.width);
+    },
+    [cache, listSize]
+  );
+
+  return (
+    <List<RowProps>
+      onResize={setListSize}
+      rowComponent={RowComponent}
+      rowCount={images.length}
+      rowHeight={rowHeight}
+      rowProps={{ cache, images }}
+    />
+  );
+}
+
+// <end>
+
+const IMAGES: string[] = [
+  "/images/animal-3546613_1280.jpg",
+  "/images/ball-bearings-1958083_1280.jpg",
+  "/images/bourke-luck-potholes-163065_1280.jpg",
+  "/images/child-1439468_1280.jpg",
+  "/images/digiart-3405596_1280.jpg",
+  "/images/electrical-cable-mess-2654084_1280.jpg",
+  "/images/elephant-8608983_1280.jpg",
+  "/images/fema-4987740_1280.jpg",
+  "/images/log-3135150_1280.jpg",
+  "/images/man-1838330_1280.jpg",
+  "/images/manipulation-2735724_1280.jpg",
+  "/images/newborn-6467761_1280.jpg",
+  "/images/old-farm-house-2096642_1280.jpg",
+  "/images/people-2557534_1280.jpg",
+  "/images/photo-1516712109157-6a67f5d73fa1.jpg",
+  "/images/photo-1562123408-fbf8cbf92c03.jpg",
+  "/images/sculpture-99484_1280.jpg",
+  "/images/sport-4765008_1280.jpg",
+  "/images/styrofoam-19493_1280.jpg",
+  "/images/trabi-328402_1280.jpg",
+  "/images/trailers-5073244_1280.jpg",
+  "/images/tub-114349_1280.jpg",
+  "/images/venus-fly-trap-3684935_1280.jpg",
+  "/images/web-5013633_1280.jpg",
+  "/images/winter-1675197_1280.jpg",
+  "/images/woman-1838149_1280.jpg"
+];
+
+function ExampleWithImages() {
+  return <Example images={IMAGES} />;
+}
+
+export { ExampleWithImages, RowComponent };
+export type { Size };
