ef27031fcc589a629d6c8e58990ddd9be72b7dd3
Brian Vaughn <brian.david.vaughn@gmail.com>
2025-10-02 18:50:58 -0400

    Version page tweaks

--- src/routes/VersionsRoute.tsx
+++ src/routes/VersionsRoute.tsx
@@ -2,6 +2,7 @@ import { Fragment } from "react/jsx-runtime";
 import { Box } from "../components/Box";
 import { ExternalLink } from "../components/ExternalLink";
 import { Header } from "../components/Header";
+import GlobeIcon from "../../public/svgs/globe.svg?react";
 import TagsIcon from "../../public/svgs/tags.svg?react";
 
 export default function VersionsRoute() {
@@ -13,7 +14,7 @@ export default function VersionsRoute() {
         .sort((a, b) => b[0].localeCompare(a[0]))
         .map(([major, minors]) => (
           <Fragment key={major}>
-            <div className="text-lg">Version {major}</div>
+            <div className="text-lg mt-2">Version {major}</div>
             <ul className="pl-8">
               {Object.entries(minors)
                 .sort((a, b) => b[0].localeCompare(a[0]))
@@ -35,29 +36,37 @@ const VERSIONS = {
   },
   "1": {
     "1.8": "https://react-window-v1.vercel.app",
-    "1.7": "https://github.com/bvaughn/react-window/releases/tag/1.7.0",
-    "1.6": "https://github.com/bvaughn/react-window/releases/tag/1.6.0",
-    "1.5": "https://github.com/bvaughn/react-window/releases/tag/1.5.0",
-    "1.4": "https://github.com/bvaughn/react-window/releases/tag/1.4.0",
-    "1.3": "https://github.com/bvaughn/react-window/releases/tag/1.3.0",
-    "1.2": "https://github.com/bvaughn/react-window/releases/tag/1.2.0",
-    "1.1": "https://github.com/bvaughn/react-window/releases/tag/1.1.0",
-    "1.0": "https://github.com/bvaughn/react-window/releases/tag/1.0.0"
+    "1.7": "",
+    "1.6": "",
+    "1.5": "",
+    "1.4": "",
+    "1.3": "",
+    "1.2": "",
+    "1.1": "",
+    "1.0": ""
   }
 };
 
 function VersionLink({ url, version }: { url: string; version: string }) {
   return (
     <li className="list-disc">
+      {version}
+      <span className="text-slate-400">.x</span>
+      {url && (
+        <ExternalLink
+          aria-label={`Documentation for version ${version}`}
+          className="ml-4"
+          href={url}
+        >
+          <GlobeIcon className="inline w-4 h-4 text-teal-200" /> documentation
+        </ExternalLink>
+      )}
       <ExternalLink
-        aria-label="Documentation for library version 1.x"
-        href={url}
+        aria-label={`GitHub tag for version ${version}`}
+        className="ml-4"
+        href={`https://github.com/bvaughn/react-window/releases/tag/${version}`}
       >
-        {version}
-        <span className="text-teal-600">.x</span>
-        {url.startsWith("https://github") && (
-          <TagsIcon className="inline w-4 h-4 ml-1 text-slate-600" />
-        )}
+        <TagsIcon className="inline w-4 h-4 text-teal-200" /> source code
       </ExternalLink>
     </li>
   );
